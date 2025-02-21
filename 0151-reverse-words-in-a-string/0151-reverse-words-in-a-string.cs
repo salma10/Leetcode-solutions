b7f311f8ff7fcc42cbcd3a7fd7c0921f420a7b4f
salma10 <salma10@users.noreply.github.com>
2025-02-21 20:01:22 +0000

    Time: 3 ms (63.23%), Space: 41.7 MB (72.06%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
@@ -1,15 +1,32 @@
 public class Solution {
     public string ReverseWords(string s) {
-        s = s.Trim();
         string[] splittedString = s.Split(" ");
         StringBuilder result = new StringBuilder();
+        int startPos = 0;
+        int endPos = splittedString.Length - 1;
+
+        for(int i = 0; i < splittedString.Length; i++)
+        {
+            if(string.IsNullOrEmpty(splittedString[i]))
+                startPos++;
+            else
+                break;
+        }
 
         for(int i = splittedString.Length - 1; i >= 0; i--)
+        {
+            if(string.IsNullOrEmpty(splittedString[i]))
+                endPos--;
+            else
+                break;
+        }
+
+        for(int i = endPos; i >= startPos; i--)
         {
             if(!string.IsNullOrEmpty(splittedString[i]))
             {
                 result.Append(splittedString[i]);
-                if(i != 0)
+                if(i != startPos)
                     result.Append(" ");
             }
         }
