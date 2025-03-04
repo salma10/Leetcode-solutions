39d75bf202437c45f2d5008ac1a063cc0be742d1
salma10 <salma10@users.noreply.github.com>
2025-03-04 20:34:41 +0000

    Time: 25 ms (33.53%), Space: 74.6 MB (99.7%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cs
+++ 0937-online-stock-span/0937-online-stock-span.cs
@@ -5,38 +5,14 @@ public class StockSpanner {
     }
     
     public int Next(int price) {
-        if(data.Count == 0)
-        {
-            int[] stData = new int[]{price,1};
-            data.Push(stData);
-            return 1;
-        }
         int result = 1;
-        int[] temp = (int[])data.Peek();
-        if(temp[0] > price)
+        while(data.Count != 0 && ((int[])data.Peek())[0] <= price)
         {
-            int[] stData = new int[]{price,result};
-            data.Push(stData);
+            result += ((int[])data.Pop())[1];
         }
-        else if(temp[0] == price)
-        {
-            result += temp[1];
-            data.Pop();
-            int[] stData = new int[]{price,result};
-            data.Push(stData);
-        }
-        else
-        {
-            while(data.Count != 0 && ((int[])data.Peek())[0] <= price)
-            {
-                result += ((int[])data.Pop())[1];
-            }
-            int[] stData = new int[]{price,result};
-            data.Push(stData);
-        }
-        
-        return result;
-        
+
+        data.Push(new int[]{price,result});       
+        return result;        
     }
 }
 
