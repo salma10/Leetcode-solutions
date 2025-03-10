9319a6052eb97d4ba4060ba766177834b28b636e
salma10 <salma10@users.noreply.github.com>
2025-03-10 20:48:06 +0000

    Time: 0 ms (100%), Space: 41.5 MB (41.64%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cs
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cs
@@ -1,19 +1,16 @@
 public class Solution {
     public string GcdOfStrings(string str1, string str2) {
-        int l1 = str1.Length, l2 = str2.Length;
-        
-        if(l2 > l1)
-            (str1, str2) = (str2, str1);
-        
-        for(int i = str2.Length; i > 0; i--)
-        {
-            string max = str2.Substring(0, i);
-            string t1 = str1.Replace(max, "");
-            string t2 = str2.Replace(max, "");
-            if(string.IsNullOrEmpty(t1) && string.IsNullOrEmpty(t2))
-                return max;
-        }
-        
-        return string.Empty;
+        if((str1 + str2) != (str2 + str1))
+            return "";
+        int gcdValue = GCD(str1.Length, str2.Length);      
+        return str2.Substring(0, gcdValue);
+    }
+
+    public int GCD(int len1, int len2)
+    {
+        if(len2 == 0)
+            return len1;
+        else
+            return GCD(len2, len1 % len2);
     }
 }
