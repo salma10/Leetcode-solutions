f72173a2f95a9bf19f807698fcc0819b9d10a5a0
salma10 <salma10@users.noreply.github.com>
2025-03-25 21:27:01 +0000

    Time: 0 ms (100%), Space: 29.2 MB (51.04%) - LeetHub

--- 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cs
+++ 1585-the-kth-factor-of-n/1585-the-kth-factor-of-n.cs
@@ -1,29 +1,14 @@
 public class Solution {
-    PriorityQueue<int, int> heap = new PriorityQueue<int, int>(new DecreaseCompare<int>());
-    public void heappushK(int x, int k) {
-        heap.Enqueue(x, x);
-        if (heap.Count > k) {
-            heap.Dequeue();    
-        }
-    }
     public int KthFactor(int n, int k) {
-        int sqrtN = (int) Math.Sqrt(n);
-        for (int x = 1; x < sqrtN + 1; ++x) {
-            if (n % x == 0) {
-                heappushK(x, k);
-                if (x != n / x) {
-                    heappushK(n / x, k);    
-                }    
-            }    
-        }  
-        return k == heap.Count ? heap.Dequeue() : -1;
-    }
-    
-    public class DecreaseCompare<T> : IComparer<T> where T : IComparable<T>
-    {
-        public int Compare(T x, T y)
+        int number = 1;
+        while(number <= n && k >= 0) 
         {
-            return y.CompareTo(x);
-        }
+            if (n % number == 0) 
+                k--;
+            if(k == 0)
+                return number;
+            number++;
+        }  
+        return -1;
     }
 }
