4aca7d1996ba14aea17fe9340902e218a7e86bbb
salma10 <salma10@users.noreply.github.com>
2025-04-11 22:24:08 +0100

    Time: 145 ms (51.76%), Space: 52.8 MB (54.68%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cs
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cs
@@ -1,26 +1,11 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
-        int shift = 0;
-        int val = nums[0];
-        int currCount = 1;
-
-        for(int i = 1; i < nums.Length; i++)
+        int count = 0;
+        for (int j = 0; j < nums.Length; j++)
         {
-            if(nums[i] == val && currCount >= 2)
-            {
-                shift++;
-            }
-            else if(nums[i] == val)
-            {
-                currCount++;
-            }
-            else
-            {
-               val =  nums[i];
-               currCount = 1;
-            } 
-            nums[i - shift] = nums[i];
+            if (count < 2 || nums[j] > nums[count-2])
+                nums[count++] = nums[j];
         }
-        return nums.Length - shift;
+        return count;
     }
 }
