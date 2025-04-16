1e20a11e478c61e23b9f5830b0fe65afd64ffdf6
salma10 <salma10@users.noreply.github.com>
2025-04-16 22:41:01 +0100

    Time: 20 ms (50.53%), Space: 44.8 MB (80.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cs
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -3,17 +3,12 @@ public class Solution {
         int left = 0;
         int right = s.Length - 1;
         s = s.ToLower();
-        HashSet<char> alphanumeric  = new HashSet<char>{
-            'a','b','c','d','e','f','g','h','i','j',
-            'k','l','m','n','o','p','q','r','s','t',
-            'u','v','w','x','y','z', '0','1','2','3',
-            '4','5','6','7','8','9'
-        };
+
         while(left <= right)
         {
-            if(!alphanumeric.Contains(s[left]))
+            if(!char.IsLetterOrDigit(s[left]))
                 left++;
-            else if(!alphanumeric.Contains(s[right]))
+            else if(!char.IsLetterOrDigit(s[right]))
                 right--;
             else if(s[left] != s[right])
                 return false;
